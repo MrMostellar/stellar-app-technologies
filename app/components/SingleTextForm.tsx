f632d115e49f9535e5e74f5e0762e895b81db02e
MrMostellar <mr.mostellar@gmail.com>
2025-01-08 22:45:01 -0700

    Fixing errors for AWS deploy test with new domain

--- app/components/SingleTextForm.tsx
+++ app/components/SingleTextForm.tsx
@@ -3,9 +3,7 @@ export default function SingleTextForm() {
     <>
       {/*form to get an email list going */}
       <form className="flex flex-col text-left min-w-[50%]">
-        <label className="p-1 pb-3">
-          <p>Let's keep in touch!</p>
-        </label>
+        <label className="p-1 pb-3">Let&#39;s keep in touch!</label>
         {/*div to style the text field*/}
         <div className="flex p-1 border-[0.0625rem] border-black rounded-[0.6rem] bg-white">
           <input
