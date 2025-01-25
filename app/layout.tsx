1ec3047e0b922751c45727eed8ec4e20ec9ce05b
MrMostellar <mr.mostellar@gmail.com>
2025-01-25 03:23:16 -0700

    added meta tag for mobile optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className="font-main text-[var(--text)] flex flex-col min-h-[100vh] overflow-y-auto">
         <header className="z-10 absolute top-0 flex flex-1 flex-row w-[100%] py-6 px-2">
           <Header />
