1ad427b8c1ac9d252430a15a1a2e0ceeb3ddf0f8
MrMostellar <mr.mostellar@gmail.com>
2025-02-11 12:23:28 -0700

    Added metadata to pages for SEO

--- app/pages/contact/page.tsx
+++ app/pages/contact/page.tsx
@@ -1,9 +1,20 @@
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Contact Us",
+  description: "Let's keep in touch!",
+};
 
 export default function ContactPage() {
   return (
-    <section className="pt-32 flex-[3] bg-[url('/images/bgSpace3.jpg')] bg-cover bg-no-repeat justify-center items-center min-h-screen">
-      <h1 className="text-center">Contact Us</h1>
-    </section>
+    <>
+      <metadata>
+        <title>Contact Us</title>
+      </metadata>
+      <section className="pt-32 flex-[3] bg-[url('/images/bgSpace3.jpg')] bg-cover bg-no-repeat justify-center items-center min-h-screen">
+        <h1 className="text-center">Contact Us</h1>
+      </section>
+    </>
   );
 }
