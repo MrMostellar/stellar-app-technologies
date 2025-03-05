dd21df1fcbecafe1a33d999050d3da23a8179ba1
MrMostellar <mr.mostellar@gmail.com>
2025-03-04 17:42:46 -0700

    Added Favicon, Logo, and animation for the logo

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,28 +1,36 @@
 import { FaBars } from "react-icons/fa";
 import NavBar from "./NavBar";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <>
-      <button className="flex flex-1 justify-center items-center p-2">
-        <Link href="/" className="titleText">
+      <button className="flex flex-1 items-center justify-center p-2">
+        <Link href="/" className="titleText flex items-center">
+          <Image
+            className="spin mx-4"
+            alt="Stellar App Technologies Logo"
+            src={"/images/FaviconAndLoader-StellarAppTechnologies.png"}
+            width={50}
+            height={50}
+          />
           Stellar App Technologies
         </Link>
       </button>
 
       <div className="items-center justify-center xs:hidden lg:flex lg:flex-1">
-        <nav className="flex flex-1 -translate-x-16 max-w-[60%]">
+        <nav className="flex max-w-[60%] flex-1 -translate-x-16">
           <NavBar />
         </nav>
       </div>
 
       <nav className="flex pr-4 xs:flex-[0.25] lg:hidden">
-        <div className="[&>nav]:hover:block [&>button]:hover:hidden flex flex-1 justify-center items-center lg:hidden">
-          <button className="flex-col block">
+        <div className="flex flex-1 items-center justify-center lg:hidden [&>button]:hover:hidden [&>nav]:hover:block">
+          <button className="block flex-col">
             <FaBars size="1.25rem" color="var(--text)" />
           </button>
-          <nav className="flex-col absolute translate-y-14 p-2 hidden ">
+          <nav className="absolute hidden translate-y-14 flex-col p-2">
             <NavBar />
           </nav>
         </div>
